f4aea7763ffe46f53bc1a6a67991c4d271acb0d3
yoongagagaga <yoongagagaga@gmail.com>
2023-03-21 16:16:06 +0900

    ADD : 3/21 _1 1. form 파일 체크박스 선택시 DB저장 작업중
    HAVE TO DO :
    1. form 파일 작업 마무리
    2. form 파일 프론트앤드 디테일
    3. 안내문 출력인 notice 창 프론트앤드 프레임

--- src/main/java/com/kh/blood/munjin/service/MunjinService.java
+++ src/main/java/com/kh/blood/munjin/service/MunjinService.java
@@ -1,9 +1,12 @@
 package com.kh.blood.munjin.service;
 
+import com.kh.blood.munjin.domain.Form;
 import com.kh.blood.munjin.domain.MunjinMember;
 
 public interface MunjinService
 {
 
  int insertMunjinMember(MunjinMember munjinMember);
+
+ String selections(Form form);
 }
